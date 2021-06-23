f8f50d62aefede8713448304930dcb77a720bf27
performanceAnalisis <performance.analisis@gmail.com>
2021-06-23 13:28:38 +0000

    v2 cuenta errores

--- cuentaEsiti.js
+++ cuentaEsiti.js
@@ -4,6 +4,9 @@ function tiempoTelLiteOK(){
 var tabla= document.getElementById("gridview-1023-table");
 var sumSegundos= 0;
 var sumSegundosOK= 0;
+var sumSegundosError= 0;
+
+
 
 var dimension= tabla.rows.length;
 
@@ -53,11 +56,11 @@ for (i = 1; i < tabla.rows.length; i++){
 
 		arr= filaStr[5].split(":");     //el 5 es el tiempo el 9 es la nota
 		callSegundos= arr[0]*3600 + arr[1]*60 + (+arr[2]);
-		sumSegundos = callSegundos + sumSegundos;
+		sumSegundosOK = callSegundos + sumSegundosOK;
 
-		hours = Math.floor(sumSegundos / 3600);
-		minutes = Math.floor((sumSegundos - (hours * 3600)) / 60);
-		seconds = sumSegundos - (hours * 3600) - (minutes * 60);
+		hours = Math.floor(sumSegundosOK / 3600);
+		minutes = Math.floor((sumSegundosOK - (hours * 3600)) / 60);
+		seconds = sumSegundosOK - (hours * 3600) - (minutes * 60);
 
 		var timeResultadoStringOK = hours.toString().padStart(2, '0') + ':' +
 			minutes.toString().padStart(2, '0') + ':' +
@@ -67,7 +70,39 @@ for (i = 1; i < tabla.rows.length; i++){
 	}
 
 
-alert("Tiempo al telefono: "+timeResultadoString+"\n"+"Cantidad de llamadas:  "+dimension+"\n"+"Tiempo OK: "+timeResultadoStringOK)
+	//-----------------------------------------------------------------------
+	//a partir de aqui codigo suma los Error
+	//if filaStr[9]==="Error" suma / else sigue
+
+	for (i = 1; i < tabla.rows.length; i++){
+
+ 
+		var fila = document.getElementById("gridview-1023-record-ext-record-"+i)  //ultimo numero es la fila desde 0
+	
+		var filaStr= fila.innerText.split("\n"); //convierto fila en string
+
+
+
+	if (filaStr[9]==="Error"){
+
+		arr= filaStr[5].split(":");     //el 5 es el tiempo el 9 es la nota
+		callSegundos= arr[0]*3600 + arr[1]*60 + (+arr[2]);
+		sumSegundosError = callSegundos + sumSegundosError;
+
+		hours = Math.floor(sumSegundosError / 3600);
+		minutes = Math.floor((sumSegundosError - (hours * 3600)) / 60);
+		seconds = sumSegundosError - (hours * 3600) - (minutes * 60);
+
+		var timeResultadoStringError = hours.toString().padStart(2, '0') + ':' +
+			minutes.toString().padStart(2, '0') + ':' +
+			seconds.toString().padStart(2, '0');
+		}
+
+	}
+
+
+
+alert("Tiempo al telefono: "+timeResultadoString+"\n"+"Cantidad de llamadas:  "+dimension+"\n"+"Tiempo OK: "+timeResultadoStringOK+"\n"+"Tiempo Error: "+timeResultadoStringError)
 
  
 }
